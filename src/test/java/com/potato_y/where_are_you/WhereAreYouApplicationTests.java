1399a4cd6364f516d06e0a988711007e766d8741
Potato-Y <dev.potatoy@gmail.com>
2024-10-25 18:27:59 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/potato_y/where_are_you/WhereAreYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potato_y.where_are_you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereAreYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
