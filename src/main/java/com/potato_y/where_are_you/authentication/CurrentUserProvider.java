db6acc7b38354c0563ef410f15f0543a8d0211ea
Potato-Y <dev.potatoy@gmail.com>
2024-10-29 01:52:50 +0900

    refactor(authentication): getCurrentUser() 예외 개선
    커스텀 예외인 `NotFoundException`으로 교체

--- src/main/java/com/potato_y/where_are_you/authentication/CurrentUserProvider.java
+++ src/main/java/com/potato_y/where_are_you/authentication/CurrentUserProvider.java
@@ -1,9 +1,9 @@
 package com.potato_y.where_are_you.authentication;
 
+import com.potato_y.where_are_you.error.exception.NotFoundException;
 import com.potato_y.where_are_you.user.domain.User;
 import com.potato_y.where_are_you.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,11 +15,11 @@ public class CurrentUserProvider {
 
   private final UserRepository userRepository;
 
-  public User getCurrentUser() throws NotFoundException {
+  public User getCurrentUser() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
     return userRepository.findByEmail(userDetails.getUsername())
-        .orElseThrow(NotFoundException::new);
+        .orElseThrow(() -> new NotFoundException("Unexpected user"));
   }
 }
--- src/main/java/com/potato_y/where_are_you/error/handler/ExceptionsHandler.java
+++ src/main/java/com/potato_y/where_are_you/error/handler/ExceptionsHandler.java
@@ -2,6 +2,7 @@ package com.potato_y.where_are_you.error.handler;
 
 import com.potato_y.where_are_you.error.dto.ErrorResponse;
 import com.potato_y.where_are_you.error.exception.BadRequestException;
+import com.potato_y.where_are_you.error.exception.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +19,12 @@ public class ExceptionsHandler {
   @ResponseStatus(HttpStatus.BAD_REQUEST) // status: 400
   public ErrorResponse handleException(BadRequestException e) {
     return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+  }
+
+  @ExceptionHandler(NotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND) // status: 404
+  public ErrorResponse handleException(NotFoundException e) {
+    return new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
   }
 
   @ExceptionHandler(Exception.class)
