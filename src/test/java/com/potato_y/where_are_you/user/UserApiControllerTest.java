a6c2d6a6314fe70033be989beecf702aed241b87
Potato-Y <dev.potatoy@gmail.com>
2024-10-31 00:13:04 +0900

    test(user) 사용자 조회 테스트 코드 추가

---
+++ src/test/java/com/potato_y/where_are_you/user/UserApiControllerTest.java
@@ -0,0 +1,63 @@
+package com.potato_y.where_are_you.user;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.potato_y.where_are_you.authentication.domain.oauth.OAuthProvider;
+import com.potato_y.where_are_you.user.domain.User;
+import com.potato_y.where_are_you.user.domain.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class UserApiControllerTest {
+
+  @Autowired
+  protected MockMvc mockMvc;
+  @Autowired
+  protected ObjectMapper objectMapper;
+  @Autowired
+  private WebApplicationContext context;
+  @Autowired
+  private UserRepository userRepository;
+
+  @BeforeEach
+  public void mockMvcSetup() {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    userRepository.deleteAll();
+  }
+
+  @DisplayName("getMyAccount(): 자신의 정보를 불러온다")
+  @WithMockUser(username = "1")
+  @Test
+  public void successGetMyAccount() throws Exception {
+    final String url = "/v1/users/me";
+    User user = userRepository.save(User.builder()
+        .oAuthProvider(OAuthProvider.KAKAO)
+        .email("user@mail.com")
+        .nickname("name")
+        .serviceId("2")
+        .build());
+
+    ResultActions result = mockMvc.perform(get(url));
+
+    result.andExpect(status().isOk())
+        .andExpect(jsonPath("$.userId").value(user.getId()))
+        .andExpect(jsonPath("$.email").value(user.getEmail()))
+        .andExpect(jsonPath("$.nickname").value(user.getNickname()));
+  }
+}
