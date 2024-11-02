471311b42c676902e3281e1c711efda7ed9d5492
Potato-Y <projectpotatoy@gmail.com>
2024-11-02 23:36:22 +0900

    feat(group): Group, GroupMember 도메인 추가
    그룹과 그룹 멤버를 관리하기 위한 Group, GroupMember Entity가 추가됩니다.
    생성 날짜를 자동으로 저장하기 위해 WhereAreYouApplication 클래스에
    EnableJpaAuditing 애너테이션이 추가됩니다.

---
+++ src/main/java/com/potato_y/where_are_you/group/domain/GroupMemberRepository.java
@@ -0,0 +1,7 @@
+package com.potato_y.where_are_you.group.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
+
+}
