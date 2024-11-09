515b673b2f8111edf87c799d93538400d0e0d3ae
Potato-Y <dev.potatoy@gmail.com>
2024-11-10 02:29:54 +0900

    feat(domain): FcmToken Entity 추가
    Fcm token을 저장할 수 있는 entity를 추가하였습니다.

---
+++ src/main/java/com/potato_y/where_are_you/firebase/domain/FcmTokenRepository.java
@@ -0,0 +1,7 @@
+package com.potato_y.where_are_you.firebase.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
+
+}
