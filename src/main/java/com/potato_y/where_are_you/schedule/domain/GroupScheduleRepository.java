17ecb44493b70b46249ee228b96d9d01819e161d
Potato-Y <dev.potatoy@gmail.com>
2024-11-11 15:51:01 +0900

    feat(domain): Group Schedule Entity 추가

---
+++ src/main/java/com/potato_y/where_are_you/schedule/domain/GroupScheduleRepository.java
@@ -0,0 +1,7 @@
+package com.potato_y.where_are_you.schedule.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupScheduleRepository extends JpaRepository<GroupSchedule, Long> {
+
+}
